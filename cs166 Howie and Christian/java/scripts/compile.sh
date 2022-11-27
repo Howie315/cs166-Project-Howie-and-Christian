68aba119d9c6614b4a0f0cf69e6f4215beb62b9d
Howie Nguyen <hnguy431@wch129-01.cs.ucr.edu>
2022-11-27 12:18:35 -0800

    added Brackets

--- cs166 Howie and Christian/java/scripts/compile.sh
+++ cs166 Howie and Christian/java/scripts/compile.sh
@@ -1,11 +1,15 @@
 #!/bin/bash
 DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
 
-export JAVA_HOME=/usr/csshare/pkgs/jdk1.7.0_17
+export JAVA_HOME=/extra/hnguy431/cs166/jdk1.8.0_352.jdk
 export PATH=$JAVA_HOME/bin:$PATH
+-Djava.awt.headless=true
+
 
 # compile the java program
 javac -d $DIR/../classes $DIR/../src/Retail.java
+
+
 
 #run the java program
 #Use your database name, port number and login
