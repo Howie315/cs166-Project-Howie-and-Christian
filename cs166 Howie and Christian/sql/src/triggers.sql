4b7f61f552b97dd2e341b924b9d46ac269e68fee
Friar-John <77084641+Friar-John@users.noreply.github.com>
2022-12-06 11:15:47 -0800

    Update triggers.sql

--- cs166 Howie and Christian/sql/src/triggers.sql
+++ cs166 Howie and Christian/sql/src/triggers.sql
@@ -31,17 +31,7 @@ create or replace function updateNextProductReqNum()
 returns trigger as
 $ord$
         begin
-
                 NEW.requestNumber = nextval('nextSupplyReqNum');
-                NEW.orderNumber = nextval('nextOrderNum');
-                NEW.orderTime = CURRENT_TIMESTAMP(0);
-                
-
-                UPDATE Product
-                SET numberOfUnits = numberOfUnits - NEW.unitsOrdered
-                WHERE NEW.storeID = storeID
-                AND NEW.productName = productName;
-
                 return NEW;
         END;
 $ord$
@@ -63,34 +53,3 @@ drop trigger if exists forProductReqs ON ProductSupplyRequests;
 create trigger forProductReqs after insert
 on ProductSupplyRequests for each row
 execute procedure updateNextProductReqNum();
-=======
---Supply request
-
-drop sequence if exists sequence_products
-CREATE SEQUENCE sequence_products START WITH 30;
-
-CREATE OR REPLACE LANAGUAGE plpgsql;
-CREATE OR REPLACE FUNCTION requestsProduct()
-RETURNS "trigger" AS
-$BODY$
-BEGIN   
-        --Inserting serials
-        NEW.requestNumber = nextval("sequence_products");
-
-        UPDATE Product 
-                SET numberOfUnits = numberOfUnits + NEW.unitsRequested
-                WHERE NEW.storeID = storeID
-                AND NEW.productNamen = productName;
-        RETURN NEW;
-END;
-$BODY$
-LANGUAGE plpgsql VOLATILE;
-
-DROP TRIGGER IF EXISTS product_requestTrigger ON ProductSupplyRequests;
-CREATE TRIGGER product_requestTrigger
-BEFORE INSERT ON ProductSupplyRequests
-FOR EACH ROW 
-EXECUTE PROCEDURE requestsProduct;
-
-
-
